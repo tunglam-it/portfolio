4afd7fcf9f31899365349ca7dc2517ee958c0c17
tunglam-it <zonakute@gmail.com>
2022-04-22 17:48:56 +0700

    csrf token

--- resources/views/partialsFE/index.blade.php
+++ resources/views/partialsFE/index.blade.php
@@ -4,6 +4,7 @@
   <meta charset="utf-8"/>
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
   <meta name="description" content=""/>
+  <meta name="csrf-token" content="{{csrf_token()}}">
   <meta name="author" content=""/>
   <title>My Portfolio</title>
   <!-- Favicon-->
