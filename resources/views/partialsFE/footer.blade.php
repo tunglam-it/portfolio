def1629ea1c35534f4f989cd8416b9e165a34326
tunglam-it <zonakute@gmail.com>
2022-04-22 22:55:42 +0700

    fix footer

--- resources/views/partialsFE/footer.blade.php
+++ resources/views/partialsFE/footer.blade.php
@@ -5,11 +5,11 @@
       <div class="col-lg-6 text-lg-start" style="font-size: 28px;font-weight: bold">Follow me on Social Media
       </div>
       <div class="col-lg-6 my-3 my-lg-0">
-        <a id="icon-media" class="btn btn-dark btn-social mx-2" href="https://www.facebook.com/imnotlam"
+        <a target="_blank" id="icon-media" class="btn btn-dark btn-social mx-2" href="https://www.facebook.com/imnotlam"
            aria-label="Facebook">
           <i class="fab fa-facebook-f"></i>
         </a>
-        <a id="icon-media" class="btn btn-dark btn-social mx-2" href="https://www.instagram.com/lamis_notlam/"
+        <a target="_blank" id="icon-media" class="btn btn-dark btn-social mx-2" href="https://www.instagram.com/lamis_notlam/"
            aria-label="Instagram">
           <i class="fab fa-instagram"></i>
         </a>
